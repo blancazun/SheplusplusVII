c012e7a464249a0511602a3ce8d9f856fbcca632
Blanca Zuñiga <bzuniganunez@gmail.com>
2024-04-19 15:04:16 -0600

    Delete comments

--- src/Arrays/Arrays.java
+++ src/Arrays/Arrays.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Arrays;
 
-/**
- *
- * @author bzuni
- */
+
 public class Arrays {
     public static void main(String[] args) {
         //Declaración
